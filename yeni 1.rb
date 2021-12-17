4c4bed67abf32472ce1c060ad93967245fc0ba3e
smikar00 <95280373+smikar00@users.noreply.github.com>
2021-12-18 01:07:33 +0300

    Add files via upload

---
+++ yeni 1.rb
@@ -0,0 +1,10 @@
+/print"Birinci Sayi:"
+fristNumber=gets.to_i
+print "Ikinci Sayiyi Girin:"
+secondNumber=gets.to_i
+a=fristNumber+secondNumber
+print "Sayilarin Toplami :",a/
+def topla(a,b,c)
+puts "Sonuc:"+(a+b+c).to_s
+end
+topla(1,2,3) 
